5081098ec79c8b31a1937df91e41bc7041c66049
OmobaVII <omoba.career@gmail.com>
2023-09-30 07:39:58 +0000

    added the no unused var to stop eslint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  /* eslint-disable no-shadow */
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
     const task = true;
+    // eslint-disable-next-line no-unused-vars
     const task2 = false;
   }
-  /* eslint-disable no-shadow */
 
   return [task, task2];
 }
