d57ab47c0e9e4fa39c53ca444da0a9b351109f4d
OmobaVII <omoba.career@gmail.com>
2023-10-05 13:56:34 +0000

    Implementing string search and join on set elements

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,8 +2,12 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
-  const valuesWithStartString = [...set].filter((value) => value.startsWith(startString));
-  const valuesWithoutString = valuesWithStartString.map((value) => value.slice(startString.length));
-  const finalString = valuesWithoutString.join('-');
-  return finalString;
+  const result = [];
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const subString = value.substring(startString.length);
+      result.push(subString);
+    }
+  }
+  return result.join('-');
 }
