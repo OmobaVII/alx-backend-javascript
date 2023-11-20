2891ab542f9d79e9946a5cc4d9d6bb7f5ff204cf
OmobaVII <omoba.career@gmail.com>
2023-11-20 18:49:09 +0000

    Implement using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const read = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+read.on('line', (input) => {
+  process.stdout.write(`Your name is: ${input}\n`);
+});
+read.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
+});
