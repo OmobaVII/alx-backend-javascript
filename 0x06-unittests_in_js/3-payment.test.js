69e458ba4ba6e618e771771b1c0d668c6763f622
OmobaVII <omoba.career@gmail.com>
2023-11-27 16:56:10 +0000

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,11 +3,13 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 const assert = require('assert');
 
-describe('sendpaymentrequesttoapi', () => {
-  it('check that Utils.calculatenumber was used', () => {
-    const sispy = sinon.spy(Utils, 'calculateNumber');
+describe('sendpaymentrequesttoapi', function() {
+  it('check that Utils.calculatenumber was used', function() => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
     sendPaymentRequestToApi(50, 23.42);
-    assert(sispy.calledOnce);
-    sispy.restore();
+
+    assert(spy.calledOnce);
+    spy.restore();
   });
 });
